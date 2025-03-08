c1936d29155a5869e0cf94af858e8333ee45aeb5
Vivien Proust <proust.viv@gmail.com>
2025-03-08 15:33:30 +0700

    qzd

--- notifier.js
+++ notifier.js
@@ -1,6 +1,7 @@
 import fetch from "node-fetch";
+import "dotenv/config";
 
-const date = "2024-05-16";
+const date = "2025-05-16";
 const moment = "noon"; // 'noon' pour midi, 'evening' pour le soir
 const pax = 4;
 
